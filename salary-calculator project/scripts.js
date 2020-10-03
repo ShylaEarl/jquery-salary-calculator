c69cc361d0381f773128d87b83c9b6b8d4cc5589
Shyla Earl <shyla.earl@gmail.com>
2020-10-03 11:02:37 -0500

    most base html set up

---
+++ salary-calculator project/scripts.js
@@ -0,0 +1,22 @@
+console.log('js');
+
+$(document).ready(onReady);
+
+function onReady(){
+    console.log('in onReady');
+
+}
+
+
+
+//employee first name, last name, ID number, job title, annual salary.
+
+//A 'Submit' button should collect the form information, 
+//store the information to calculate monthly costs, 
+//append information to the DOM 
+//clear the input fields. 
+
+//Using the stored information, calculate monthly costs 
+//append this to the to DOM. 
+
+//If the total monthly cost exceeds $20,000, add a red background color to total monthly cost.
