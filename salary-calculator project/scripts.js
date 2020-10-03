114906fd6a7c15e9534993db0767ee168642f02e
Shyla Earl <shyla.earl@gmail.com>
2020-10-03 12:23:13 -0500

    working on table

--- salary-calculator project/scripts.js
+++ salary-calculator project/scripts.js
@@ -30,6 +30,8 @@ function submitEmInput(){
     emInput.push(employeeInput);
     console.log('in submitEmInput', emInput);
 
+    //append information to the DOM. Where am I appending it too? 
+
     $('#firstName').val('');
     $('#lastName').val('');
     $('#id').val('');
@@ -38,14 +40,26 @@ function submitEmInput(){
 
 }
 
-//employee first name, last name, ID number, job title, annual salary.
+function deleteEmInput(){
+    console.log('in deleteEmInput');
+    
 
-//A 'Submit' button should collect the form information, 
-//store the information to calculate monthly costs, 
+}
+
+//employee first name, last name, ID number, job title, annual salary. CHECK!
+
+//A 'Submit' button should collect the form information, CHECK!
+//store the information to calculate monthly costs, CHECK!
 //append information to the DOM 
-//clear the input fields. 
+//clear the input fields. CHECK!
 
 //Using the stored information, calculate monthly costs 
 //append this to the to DOM. 
 
 //If the total monthly cost exceeds $20,000, add a red background color to total monthly cost.
+
+//may need/want to create a table function 
+
+//let table = $('#create in html and add id here')
+//let row = table.insertRow(0);
+//let cell = row.insertcell();
