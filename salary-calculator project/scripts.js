5fbaacb4a7544c68eef6a00a31ae639c4f46c371
Shyla Earl <shyla.earl@gmail.com>
2020-10-03 11:25:38 -0500

    submitEmInput function working

--- salary-calculator project/scripts.js
+++ salary-calculator project/scripts.js
@@ -2,12 +2,41 @@ console.log('js');
 
 $(document).ready(onReady);
 
+let emInput = [];
+
 function onReady(){
     console.log('in onReady');
 
+    $('#submitEmInput').on('click', submitEmInput);
+
 }
 
+function submitEmInput(){
 
+    let firstName = $('#firstName').val();
+    let lastName = $('#lastName').val();
+    let id = $('#id').val();
+    let title = $('#title').val();
+    let annualSalary = $('#annualSalary').val();
+
+    let employeeInput = {
+        firstName: firstName,
+        lastName: lastName,
+        id: id, 
+        title: title,
+        annualSalary: annualSalary
+    }
+
+    emInput.push(employeeInput);
+    console.log('in submitEmInput', emInput);
+
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#id').val('');
+    $('#title').val('');
+    $('#annualSalary').val('');
+
+}
 
 //employee first name, last name, ID number, job title, annual salary.
 
